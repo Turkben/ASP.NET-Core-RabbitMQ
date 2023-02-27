e9d09c99cea6e5392dea515d8aaa283a1aefe7f4
Gökhan Türkben <gokturk2002@hotmail.com>
2023-02-27 15:22:02 +0300

    file controller api created

--- RabbitMQ.ExcelApp.ConsumerWorkerService/Program.cs
+++ RabbitMQ.ExcelApp.ConsumerWorkerService/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
 using RabbitMQ.ExcelApp.ConsumerWorkerService;
+using RabbitMQ.ExcelApp.ConsumerWorkerService.Models;
 using RabbitMQ.ExcelApp.ConsumerWorkerService.Services;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext,services) =>
     {
         IConfiguration configuration = hostContext.Configuration;
+
+        services.AddDbContext<AdventureWorks2019Context>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SqlConnection"));
+        });
+
         services.AddSingleton(sp => {
             var connectionString = configuration.GetConnectionString("RabbitMQ");
             return new ConnectionFactory()
---
+++ RabbitMQ.ExcelApp/Controllers/FilesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.ExcelApp.Models;
+
+namespace RabbitMQ.ExcelApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public FilesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file, int fileId)
+        {
+            //if(file.Length> = 0)
+            if (file is not { Length:>0})
+            {
+                return BadRequest();
+            }
+
+            var userFile = await _appDbContext.UserFiles.FirstAsync(f => f.Id == fileId);
+            var filePath = userFile?.FileName + Path.GetExtension(file.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/files", filePath);
+
+            using FileStream stream = new FileStream(path, FileMode.Create);
+            await file.CopyToAsync(stream);
+
+            userFile.CreatedDate = DateTime.Now;
+            userFile.FilePath = filePath;
+            userFile.FileStatus = FileStatus.Completed;
+
+            await _appDbContext.SaveChangesAsync();
+
+            //todo: signalr notfication
+
+            return Ok();
+        }
+    }
+}
